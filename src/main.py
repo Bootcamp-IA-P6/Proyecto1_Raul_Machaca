550c33e64a912b2651dd44f478ed08135584995a
RaulCtm <brunoraulmachaca@gmail.com>
2025-12-10 15:41:07 +0100

    feat(cli): agregar mensaje de bienvenida (#4)

---
+++ src/main.py
@@ -0,0 +1,7 @@
+def taximeter():
+    # Mensaje de bienvenida
+    print("Welcome to the F5 Taximeter!")
+    print("Available commands: 'start', 'stop', 'move', 'finish', 'exit'\n")
+
+if __name__ == "__main__":
+    taximeter()
