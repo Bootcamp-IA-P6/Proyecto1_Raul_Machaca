ccfc5d21d8ff08394377be9a45ab62009351d806
RaulCtm <brunoraulmachaca@gmail.com>
2025-12-15 05:22:41 +0100

    bug(web): Se añadieron imports en app.py para ejecutar correctamente en web (#27)

--- web/app.py
+++ web/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import streamlit as st
 from taximeter import Trip
 from src.auth import authenticate
